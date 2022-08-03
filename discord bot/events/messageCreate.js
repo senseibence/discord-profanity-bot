fa4d6ad6335bafae8fbe899e40c7b65b01574780
Bence Lukácsy <72169848+senseibence@users.noreply.github.com>
2022-08-02 21:09:18 -0400

    Update messageCreate.js

--- discord bot/events/messageCreate.js
+++ discord bot/events/messageCreate.js
@@ -1,10 +1,21 @@
+// this file checks if a new message was created and then calls on our profanity function if there was 
+
 const fromFilterjs = require('../commands/filter.js');
 const fromProfanityjs = require('../isProfanityFunction.js');
 
 module.exports = {
 	name: 'messageCreate',
 	execute(msg) {
+	
+	/*
 
+	these arrays and variables are used to split each discord server's bot settings
+	for example, server#1 might want the bot turned on whilst server#2 might want it off
+	without this setup, there would only be one global boolean for whether the bot was on or off
+	(we also need to do this for bot modes, strict and lenient)
+	
+	*/
+		
         const currentGuildId = msg.guild.id;
         const guildIdsArray = fromFilterjs.getGuildIds();
         const guildStatusArray = fromFilterjs.getGuildOnStatus();
