417fc63c2f740218ebd54217655e1dc682c7a189
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-10-02 16:02:10 -0400

    Update interactionCreate.js

--- discord bot homemade word list/events/interactionCreate.js
+++ discord bot homemade word list/events/interactionCreate.js
@@ -8,7 +8,19 @@ module.exports = {
         if (!command) return;
 
         try {
-            return command.execute(interaction);
+            if (interaction.guild.id === desiredserver) {
+                if (interaction.channel.id === desiredchannel) {
+                    return command.execute(interaction);
+                }
+
+                else {
+                    interaction.reply("You do not have permission to use this command!");
+                }
+            }
+
+            else {
+                return command.execute(interaction);
+            }
         } catch (error) {
             console.error(error);
             return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
