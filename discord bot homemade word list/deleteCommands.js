9c688d7ae5c1487c46462297643165473d637ec9
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-10-02 14:30:00 -0400

    Update deleteCommands.js

--- discord bot homemade word list/deleteCommands.js
+++ discord bot homemade word list/deleteCommands.js
@@ -1,17 +1,17 @@
-// When guildId is used, removes guild commands
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require("./strings.json");
     
 const rest = new REST({ version: '9' }).setToken(token);
-rest.get(Routes.applicationGuildCommands(clientId, guildId))
-    .then(data => {
-        const promises = [];
-        for (const command of data) {
-            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
-            promises.push(rest.delete(deleteUrl));
-        }
-        return Promise.all(promises);
-    });
+
+// for guild-based commands
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+
+// for global commands
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
