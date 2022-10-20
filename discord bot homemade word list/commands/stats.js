91fdf48a2798aef3adc648a37b5529ff13b51f3e
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-10-19 20:01:22 -0400

    Update stats.js

--- discord bot homemade word list/commands/stats.js
+++ discord bot homemade word list/commands/stats.js
@@ -1,18 +1,30 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fromIsProfanityjs = require('../isProfanityFunction.js');
 const fromMessageCreatejs = require('../events/messageCreate');
+const fromFilterjs = require('./filter.js');
+const GuildSettings = fromFilterjs.getGuildSettings();
+const { MessageEmbed } = require('discord.js');
 let client;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stats')
 		.setDescription('Bot stats')
-		.setDMPermission(true)
-		.setDefaultMemberPermissions(0), 
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(0), 
         
 	async execute(interaction) {
 		client = interaction.client;
+		const guildMap = fromFilterjs.getGuildMap();
+		const allGuildIds = client.guilds.cache.map(guild => guild.id);
 
+		for (let i = 0; i < allGuildIds.length; i++) {
+			let currentGuildId = allGuildIds[i];
+			if (!guildMap.has(currentGuildId)) {
+				guildMap.set(currentGuildId, new GuildSettings());
+			}
+		}
+		
 		let totalSeconds = (client.uptime / 1000);
 		let days = Math.floor(totalSeconds / 86400);
 		totalSeconds %= 86400;
@@ -26,13 +38,12 @@ module.exports = {
 		let totalMessages = fromMessageCreatejs.getTotalMessages();
 		let totalDeleted = fromIsProfanityjs.getTotalDeleted();
 		
-		// everything from the client gets updated correctly except member count. So the below correctly gives uptime, server count, and server name, but not member count. I have no fix
-		return interaction.reply({ 
+        return interaction.reply({ 
 			embeds: [
 			  new MessageEmbed()
-			  .setDescription("Messages Interpreted: "+totalMessages+'\n'+"Profanities Deleted: "+totalDeleted+'\n'+"Uptime: "+uptime+'\n'+"Server Count: "+client.guilds.cache.size+'\n'+"Total Member Count: "+client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)+'\n\n'+
+			  .setDescription("Interpreted: "+totalMessages+'\n'+"Profanities Deleted: "+totalDeleted+'\n'+"Uptime: "+uptime+'\n'+"Server Count: "+client.guilds.cache.size+'\n'+"Total Member Count: "+client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)+'\n\n'+
 				client.guilds.cache
-				  .map(g => `Guild Name: ${g.name}\n  Total Members: ${g.memberCount}\n Guild ID: ${g.id}`).join('\n\n')
+				  .map(guild => `Guild Name: ${guild.name}\n  Total Members: ${guild.memberCount}\n Guild ID: ${guild.id}\n Filter Status: ${guildMap.get(guild.id).guildOnStatus}`).join('\n\n') 
 			  )
 			] 
 		})
