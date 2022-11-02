5297341f39e6a42ce9a1fe325d97b11412ada5f9
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-11-02 10:40:17 -0400

    Update guildCreate.js

--- discord bot with mongoDB/events/guildCreate.js
+++ discord bot with mongoDB/events/guildCreate.js
@@ -2,24 +2,16 @@ const Guild = require('../schema.js');
 const lists = require('../profanityList.js');
 const blacklist = lists.getBlacklist();
 const whitelist = lists.getWhitelist();
-const fromReadyjs = require('../events/ready.js');
 
 module.exports = {
 	name: 'guildCreate',
 	async execute(guild) {
-		// console.log(`Joined new guild: ${guild.name}`);
-        let guildProfile = await new Guild({
-            guildId: guild.id,
-            guildOnStatus: false,
-            guildBlacklist: blacklist.slice(0),
-            guildWhitelist: whitelist.slice(0)
-        })
-        await guildProfile.save().catch(console.error);
-        const guildMap = fromReadyjs.getGuildMap();
-        guildMap.set(guild.id, {
-            onStatus: false,
-            blacklist: new Set(blacklist), 
-            whitelist: new Set(whitelist),
-        })
+		let guildProfile = await new Guild({
+		    guildId: guild.id,
+		    guildOnStatus: false,
+		    guildBlacklist: blacklist.slice(0),
+		    guildWhitelist: whitelist.slice(0)
+		})
+		await guildProfile.save().catch(console.error);
 	},
 };
