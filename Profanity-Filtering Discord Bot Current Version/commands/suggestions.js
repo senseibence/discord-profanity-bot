b4fa1d3365bc5df76fbd6d864e68d9335d9072e3
Bence Lukácsy <bence.lukacsy@gmail.com>
2022-12-10 22:22:12 -0500

    Update suggestions.js
    removed unnecessary function

--- Profanity-Filtering Discord Bot Current Version/commands/suggestions.js
+++ Profanity-Filtering Discord Bot Current Version/commands/suggestions.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const nodemailer = require('nodemailer');
 
 module.exports = {
+	
 	data: new SlashCommandBuilder()
 		.setName('suggestion')
 		.setDescription('Send suggestion')
@@ -10,39 +11,35 @@ module.exports = {
         .addStringOption(option => option.setName('suggestion').setDescription('Send suggestion')),
         
 	async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true }); // emailing takes a moment
-        
-        const suggestion = interaction.options.getString('suggestion');
-        let reply = "Suggestion received!";
-        sendMail()
+		await interaction.deferReply({ ephemeral: true }); // emailing takes a moment
 
-        async function sendMail() {
+		const suggestion = interaction.options.getString('suggestion');
+		let reply = "Suggestion received!";
 
-            try {
-                const transport = nodemailer.createTransport({
-                    service: 'hotmail', // gmail no longer works without oauth2
-                    auth: {
-                        user: 'profanityfilter1@hotmail.com',
-                        pass: // your hotmail password
-                    }
-                })
+		try {
+			const transport = nodemailer.createTransport({
+			    service: 'hotmail', // gmail no longer works without oauth2
+			    auth: {
+				user: 'profanityfilter1@hotmail.com',
+				pass: // your hotmail password
+			    }
+			})
 
-                const mailOptions = {
-                    from: '"Profanity Filter" <profanityfilter1@hotmail.com>',
-                    to: 'profanityfilter1@hotmail.com',
-                    subject: 'Suggestion',
-                    text: suggestion
-                };
+			const mailOptions = {
+			    from: '"Profanity Filter" <profanityfilter1@hotmail.com>',
+			    to: 'profanityfilter1@hotmail.com',
+			    subject: 'Suggestion',
+			    text: suggestion
+			};
 
-                await transport.sendMail(mailOptions);
+			await transport.sendMail(mailOptions);
 
-            } catch (error) {
-                reply = "An error occured while replying to this command";
-                console.error(error);
-                
-            } finally {
-                await interaction.editReply(reply);
-            }
-        }
+		} catch (error) {
+			reply = "An error occured while replying to this command";
+			console.error(error);
+
+		} finally {
+			await interaction.editReply(reply);
+		}
 	},
 };
