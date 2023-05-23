5d4dd8c3b39707fcce1d09e43624ba18c5dcc16a
Bence Lukácsy <bence.lukacsy@gmail.com>
2023-05-22 22:45:33 -0400

    Delete discord bot directory

--- discord bot/events/ready.js
+++
@@ -1,9 +0,0 @@
-// this file turns the bot on and prints a ready message
-
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
